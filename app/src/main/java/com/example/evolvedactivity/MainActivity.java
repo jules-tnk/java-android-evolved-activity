5aad1f0cece9fe21f5bf21a84c64f038add01eb9
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2022-12-28 18:54:39 +0100

    Add web page redirection option to main activity Add PersoActivity and navigation to it

--- app/src/main/java/com/example/evolvedactivity/MainActivity.java
+++ app/src/main/java/com/example/evolvedactivity/MainActivity.java
@@ -1,26 +1,46 @@
 package com.example.evolvedactivity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.app.ProgressDialog.show;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton launchPhoneCallButton;
+    ImageButton openWebPageButton;
+    ImageButton openPersoActivityButton;
     EditText phoneNumberEditText;
+    EditText urlEditText;
+
+
+    int CALL_Perm = 1;
+    String defaultUrl = "https://www.emi.ac.ma/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CALL_PHONE}, CALL_Perm);
+
         launchPhoneCallButton = (ImageButton) findViewById(R.id.LaunchPhoneCallButton);
         phoneNumberEditText = (EditText) findViewById(R.id.editTextPhone);
+
+        openWebPageButton = (ImageButton) findViewById(R.id.openWebPageButton);
+        urlEditText = (EditText) findViewById(R.id.urlEditText);
+
+        openPersoActivityButton = (ImageButton) findViewById(R.id.openPersoActivityButton);
+
 
         launchPhoneCallButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -36,5 +56,42 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        openWebPageButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String url = urlEditText.getText().toString();
+                        if (url.equals("")){
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(defaultUrl)));
+                        } else {
+                            String completeUrl = "https://"+url;
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(completeUrl)));
+                        }
+                    }
+                }
+        );
+
+        openPersoActivityButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent persoActivityIntent = new Intent(getApplicationContext(), PersoActivity.class);
+                        startActivity(persoActivityIntent);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //check the permission type using the requestCode
+        if (requestCode == CALL_Perm) {
+            //the array is empty if not granted
+            if (grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "GRANTED CALL", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
